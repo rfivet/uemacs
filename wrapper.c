68a79430e621ee0fc19e9c36783f25586b1ca11e
Renaud Fivet <Renaud.Fivet@gmail.com>
2013-04-30 17:24:46 +0800

    cleanup usage and wrapper

--- wrapper.c
+++ wrapper.c
@@ -1,22 +1,29 @@
+/* wrapper.c -- implements wrapper.h */
+
+#include "wrapper.h"
+
 #include "usage.h"
 
 #include <stdlib.h>
+#include <unistd.h>
 
 /* Function copyright: git */
-int xmkstemp(char *template)
-{
-	int fd;
+void xmkstemp( char *template) {
+	int fd ;
 
-	fd = mkstemp(template);
-	if (fd < 0)
-		die("Unable to create temporary file");
-	return fd;
+	fd = mkstemp( template) ;
+	if( fd < 0)
+		die( "Unable to create temporary file") ;
+
+	close( fd) ;
 }
 
-void *xmalloc(size_t size)
-{
-	void *ret = malloc(size);
-	if (!ret)
-		die("Out of memory");
-	return ret;
+void *xmalloc( size_t size) {
+	void *ret = malloc( size) ;
+	if( !ret)
+		die( "Out of memory") ;
+
+	return ret ;
 }
+
+/* end of wrapper.c */
