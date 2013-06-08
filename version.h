a2804f6ca5a3f3699483bc47388577f34a182993
Renaud Fivet <renaud.fivet@gmail.com>
2013-06-08 10:23:23 +0800

    Align program name between Makefile and version.h

--- version.h
+++ version.h
@@ -1,7 +1,14 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define PROGRAM_NAME "ue"
+#ifdef PROGRAM
+# define _QUOTE( s) #s
+# define QUOTE( s) _QUOTE( s)
+# define PROGRAM_NAME QUOTE(PROGRAM)
+#else
+# define PROGRAM_NAME "em"
+#endif
+
 #define PROGRAM_NAME_LONG "uEMACS"
 
 #define VERSION "4.1.1"
