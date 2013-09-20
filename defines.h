39e23cb169e94a6c93e3c35b2e6d299a74ea9569
Renaud Fivet <renaud.fivet@gmail.com>
2013-09-20 11:47:47 +0800

    Defines.h missing in previous commit.

---
+++ defines.h
@@ -0,0 +1,8 @@
+#define USG 1
+
+#define	CRYPT	1  /* file encryption enabled?                     */
+
+#define	NSTRING	128		/* # of bytes, string buffers   */
+
+#define PKCODE 1
+#define ENVFUNC 1
