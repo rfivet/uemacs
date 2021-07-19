f0fe1ec19496d4fe2e979ab53abdaf8f24547faf
Renaud Fivet <renaud.fivet@gmail.com>
2021-07-19 16:37:57 +0800

    Avoid extra empty line at EOF.

--- screensize.cmd
+++ screensize.cmd
@@ -3,8 +3,7 @@ insert-string &cat $curwidth &cat "x" $pagelen
 insert-string &rig "---------+" &sub 10 $curcol
 &sub &div $curwidth 10 1 insert-string "---------+"
 insert-string &lef "1234567890" &mod $curwidth 10
-end-of-file
-&sub $pagelen 3 execute-command-line "insert-string &cat $curline ~n"
+&sub $pagelen 3 execute-command-line "insert-string &cat ~n &add $curline 1"
 beginning-of-file
 unmark-buffer
 write-message $line
